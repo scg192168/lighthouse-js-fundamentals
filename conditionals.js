6b09b911441bf9c50b8029644aa82d70c98483d9
Shyamal <scghoshbc@gmail.com>
2023-03-03 18:06:33 -0800

    commit

---
+++ conditionals.js
@@ -0,0 +1,15 @@
+touch conditionals.js
+const raining = ture;
+const cold = false;
+if (raining) {
+    console.log("Don't forget your umbrella");
+}
+
+if (cold) {
+    console.log("Make sure you pick pot a scarf!");
+}
+
+console.log("Now you're ready to go outside!");
+
+
+
