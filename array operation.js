a506d257d360de0dabc9e174af871f7d9c7e7e72
scg192168 <scghoshbc@gmail.com>
2023-03-05 13:19:55 -0800

    Create array operation.js

---
+++ array operation.js
@@ -0,0 +1,27 @@
+const director = "Steven Spielberg";
+const film1 = "Jaws";
+const film2 = "Raiders of the Lost Ark";
+const film3 = "A.I.";
+const director = "Steven Spielberg";
+const films = ["Jaws", "Raiders of the Lost Ark", "A.I."];
+"My favourite " + director + " film is " + films[1];
+"films at index 0: " + films[0];
+"films at index 1: " + films[1];
+"films at index 2: " + films[2];
+"films at index 3: " + films[3];
+const oddnumber = [1, 3, 5, 7, 9];
+const randomStuff = [1.15, "bicycle", true];
+
+
+//exit and re-enter the node REPL//
+//enter the following commands//
+
+const director = "Steven Spielberg';
+const films = ["Jaws", "Raiders of the Lost Ark", "A.I."];
+films[2] = "Jurassic Park";
+films;
+films.push("A.I.");
+films;
+director + " is the director of " + films.length + " films";
+films.push("E.T.");
+director + " is the director of " + films.length + " films";
