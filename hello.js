0b7983ee82ddd79502634ff514674dc9bb067a95
scg192168 <scghoshbc@gmail.com>
2023-03-04 23:28:38 -0800

    Create hello.js

---
+++ hello.js
@@ -0,0 +1,18 @@
+const sayHello = function () {
+  console.log("Hello, world");
+  
+}
+
+sayHello();
+
+
+const sayHello = function (name) {
+  console.log("Hello, " + name);
+  
+}
+
+
+sayHello("Caliban");
+sayHello("Miranda");
+sayHello("Ferdinand");
+
