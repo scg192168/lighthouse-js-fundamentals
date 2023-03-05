69b1b3604c248d10d55e55dc49523a38d4ec8882
scg192168 <scghoshbc@gmail.com>
2023-03-05 15:13:00 -0800

    Create howmanyhundreds.js

---
+++ howmanyhundreds.js
@@ -0,0 +1,10 @@
+function howManyHundreds(num) {
+  return((num / 100) - (num % 100) / 100);
+}
+
+console.log(howManyHundreds(1000);
+console.log(howManyHundreds(894);
+console.log(howManyHundreds(520);
+console.log(howManyHundreds(99);
+console.log(howManyHundreds(0);
+            
