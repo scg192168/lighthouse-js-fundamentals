cc3a4d6d8cb9be3390c831e7fef6678910dd3e38
scg192168 <scghoshbc@gmail.com>
2023-03-07 11:57:40 -0800

    Create sumLargestNumbers.js

---
+++ sumLargestNumbers.js
@@ -0,0 +1,7 @@
+const sumLargestNumbers = function(numArray) {
+  numArray.sort ((a, b) => b - a));
+  return numArr[0] + numArr[1];
+};
+console.log(sumLargestNumbers([1, 10]));
+console.log(sumLargestNumbers([1, 2, 3]));
+console.log(sumLargestNumbers([10, 4, 34, 6, 92, 2]));
