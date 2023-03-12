0d393e8f09e4751643fd5aa2803f8b0b97e25586
scg192168 <scghoshbc@gmail.com>
2023-03-11 23:35:54 -0800

    Create mergeArrays.js

---
+++ mergeArrays.js
@@ -0,0 +1,7 @@
+function merge(arr1, arr2) {
+  return arr1.concat(arr2).sort((a, b) => a - b);
+}
+
+console.log(merge([ 4, 5, 6 ], [ 1, 2, 3, 4 ]), "=?", [ 1, 2, 3, 4, 4, 5, 6 ]);
+console.log(merge([ 4 ], [ 2, 5, 8 ]), "=?", [ 2, 4, 5, 8 ]);
+console.log(merge([ 1, 2, 6 ], []), "=?", [ 1, 2, 6 ]);
