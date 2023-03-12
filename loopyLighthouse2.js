c6dd1c61db3bffdae459dd24df0056ab49d40621
scg192168 <scghoshbc@gmail.com>
2023-03-11 23:33:16 -0800

    Create loopyLighthouse2.js

---
+++ loopyLighthouse2.js
@@ -0,0 +1,13 @@
+const loopyLighthouse = (range, multiples, words) => {
+
+  for (let i = range[0]; i <= range[1]; i++) {
+    let output = '';
+
+    if (i % multiples[0] === 0) output += words[0];
+    if (i % multiples[1] === 0) output += words[1];
+
+    console.log(output || i);
+  }
+
+}
+loopyLighthouse([15, 90], [2, 5], ['Batty', 'Beacon']);
